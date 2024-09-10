e24392f83dc64e5923a7cb0eec5b248842671b4e
angelo bessolo <angelobessolo@gmail.com>
2024-09-10 17:11:39 -0500

    Actualizacion back

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+ 
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +13,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
+
+  app.enableCors();
 
   // await app.listen(3000);
   const port = process.env.PORT || 3000;
